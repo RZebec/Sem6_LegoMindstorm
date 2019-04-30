aa1f21f21334b2a65c6e4a41e570799f9d14c63c
Robert Zebec <zebec.robert@gmail.com>
2019-04-30 11:33:15 +0200

    Follower und Leader

--- self_study/follower/main.py
+++ self_study/follower/main.py
@@ -12,25 +12,50 @@ left_motor = Motor(Port.A)
 right_motor = Motor(Port.B)
 wheel_diameter = 56
 axle_track = 114
-max_distance = 100
+max_distance = 400
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
 sensor = UltrasonicSensor(Port.S1)
+gyroSensor = GyroSensor(Port.S3)
+isDriving = False
 
+
+speed = 130
+max_angle = 3
+adjustment_time = 350
 
 def motor_start():
-    robot.drive(100,0)
+    robot.drive(speed,0)
 
 def motor_stop():
     robot.stop()
 
 def read_signal():
+    global isDriving
     currentDistance = sensor.distance()
     if(currentDistance > max_distance):
         motor_start()
+        isDriving = True
     else:
         motor_stop()
+        isDriving = False
+
+def adjust_direction():  
+    global isDriving
+    if(isDriving):
+        curAngle = gyroSensor.angle()
+        if (curAngle > max_angle):
+            robot.drive_time(speed, -5, adjustment_time)
+            brick.display.text("Adjusting")
+        if (curAngle < -max_angle):
+            robot.drive_time(speed, 5, adjustment_time)
+            brick.display.text("Adjusting")
     
+
+max_distance = sensor.distance()
+gyroSensor.reset_angle(0)
 while True:
+    
+    adjust_direction()
     read_signal()
     wait(10)
--- self_study/leader/main.py
+++ self_study/leader/main.py
@@ -12,25 +12,54 @@ left_motor = Motor(Port.A)
 right_motor = Motor(Port.B)
 wheel_diameter = 56
 axle_track = 114
+ir_sensor = InfraredSensor(Port.S4)
+gyroSensor = GyroSensor(Port.S3)
+
+
+speed = 130
+max_angle = 3
+adjustment_time = 350
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
-ir_sensor = InfraredSensor(Port.S1)
+
+isDriving = False
 
 
 def motor_start():
-    robot.drive(100,0)
+    robot.drive(speed,0)
 
 def motor_stop():
     robot.stop()
 
+def adjust_direction():  
+    if(isDriving):
+        curAngle = gyroSensor.angle()
+        if (curAngle > max_angle):
+            robot.drive_time(speed, -5, adjustment_time)
+            motor_start()
+            brick.display.text("Adjusting")
+        if (curAngle < -max_angle):
+            robot.drive_time(speed, 5, adjustment_time)
+            brick.display.text("Adjusting")
+            motor_start()
+
 def read_ir_signal():
+    global isDriving
     listOfPressedButtons = ir_sensor.buttons(1)
-    if (Button.UP in listOfPressedButtons):
+    print(listOfPressedButtons)
+    if (Button.LEFT_UP in listOfPressedButtons):
+        brick.display.text("UP")
         motor_start()
+        isDriving = True
         return
-    if(Button.DOWN in listOfPressedButtons):
+    if(Button.LEFT_DOWN in listOfPressedButtons):
+        brick.display.text("DOWN")
         motor_stop()
+        isDriving = False
         return
+
 while True:
     read_ir_signal()
+    print(isDriving)
+    adjust_direction()
     wait(10)
