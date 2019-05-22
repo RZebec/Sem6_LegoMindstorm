79be677c8463662afce4f6bea05034e16bdeb51f
André Berberich <Andre.Berberich@tecalliance.net>
2019-05-22 13:41:40 +0200

    Added comments to the follower code

--- self_study/follower/main.py
+++ self_study/follower/main.py
@@ -8,30 +8,37 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
 
+# Configuration, which depends on the build roboter:
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.B)
-wheel_diameter = 56
-axle_track = 114
-max_distance = 400
-
-robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
 sensor = UltrasonicSensor(Port.S1)
 gyroSensor = GyroSensor(Port.S3)
-isDriving = False
+wheel_diameter = 56
+axle_track = 114
 
-
-speed = 130
+# Configuration regarding the behavior of the robot:
+speed = 130 #
 max_angle = 3
 adjustment_time = 350
+max_distance = 400
 
+# Global variables:
+robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
+isDriving = False
+
+# Start the motor and let the robot drive:
 def motor_start():
     robot.drive(speed,0)
 
+# Stop the motor and the robot:
 def motor_stop():
     robot.stop()
 
+# Read the ultrasonic sensor to get the distance:
 def read_signal():
+    # global modifier, because we need to change the variable:
     global isDriving
+    # Get the current distance and react:
     currentDistance = sensor.distance()
     if(currentDistance > max_distance):
         motor_start()
@@ -40,9 +47,12 @@ def read_signal():
         motor_stop()
         isDriving = False
 
+# Read the gyro sensor and adjust the direction of the robot:
 def adjust_direction():  
+    # Only adjust the direction when the robot is moving:
     global isDriving
     if(isDriving):
+        # Get the current angle and compare it with the initial angle:
         curAngle = gyroSensor.angle()
         if (curAngle > max_angle):
             robot.drive_time(speed, -5, adjustment_time)
@@ -51,11 +61,22 @@ def adjust_direction():
             robot.drive_time(speed, 5, adjustment_time)
             brick.display.text("Adjusting")
     
+# The following values will be set during the start of the application:
 
+# Get the current distance to the robot in front of the sensor:
+# The value will be dynamically set during the application start:
 max_distance = sensor.distance()
+
+# Set the angle of the gyroscope to zero. The main loop will 
+# evaluate the value and adjust the direction of the robot:
 gyroSensor.reset_angle(0)
-while True:
-    
+
+# The main loop which orchestrates the different functions:
+while True:   
+    # Adjust the direction in comparison to the angle of the gyroscope,
+    # which is set during the start of the application: 
     adjust_direction()
+    # Read the distance signal and start or stop the robot:
     read_signal()
+    # Wait 10 milliseconds till the next iteration:
     wait(10)
